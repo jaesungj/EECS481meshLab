e784a51aca3a7bffc1e01c2cbe1a70c3d774cee9
ubuntu <ubuntu@ubuntu.(none)>
2015-11-17 18:18:55 -0500

    added vcg

--- vcglib/wrap/ply/plystuff.h
+++ vcglib/wrap/ply/plystuff.h
@@ -59,6 +59,8 @@ Cleaning of the automatic bbox caching support for ply files. First working vers
 #include <fcntl.h> 
 #ifdef WIN32
 #include <io.h>
+#else
+#include <unistd.h>
 #endif
 
 #include <vcg/space/box3.h>
